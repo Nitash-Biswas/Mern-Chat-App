4cf3f81a70521994050c7f1f414a752e5a0d9fec
Nitash-Biswas <nitash.nb@gmail.com>
2024-07-29 19:37:34 +0530

    Mern App First Commit

---
+++ frontend/src/components/messages/MessageInput.jsx
@@ -0,0 +1,91 @@
+import { useState } from "react";
+import { BsSend } from "react-icons/bs";
+import { MdEmojiEmotions } from "react-icons/md";
+import useSendMessage from "../../hooks/useSendMessage";
+import Picker from "emoji-picker-react";
+
+const MessageInput = () => {
+  const [message, setMessage] = useState("");
+  const { loading, sendMessage } = useSendMessage();
+  const [showEmojiPicker, setShowEmojiPicker] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!message) return;
+    await sendMessage(message);
+    setMessage("");
+  };
+
+  const handleEmojiPickerhideShow = () => {
+    setShowEmojiPicker(!showEmojiPicker);
+  };
+
+  const handleEmojiClick = (event, emojiObject) => {
+    console.log(emojiObject);
+    setMessage(message + emojiObject.emoji);
+  };
+
+  return (
+    <form
+      className="px-2 pr-4 my-4 flex gap-2 items-center"
+      onSubmit={handleSubmit}
+    >
+      <div className="text-3xl relative">
+        <MdEmojiEmotions onClick={handleEmojiPickerhideShow} className="cursor-pointer hover:text-white" />
+        {showEmojiPicker && (
+          <Picker
+            onEmojiClick={handleEmojiClick}
+            pickerStyle={{
+              position: "absolute",
+              top: "-340px",
+
+            }}
+			disableSearchBar
+
+          />
+        )}
+      </div>
+      <div className="w-full relative">
+        <input
+          type="text"
+          className="border text-sm rounded-lg block w-full p-2.5  bg-gray-700 border-gray-600 text-white"
+          placeholder="Send a message"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <button
+          type="submit"
+          className="absolute inset-y-0 end-0 flex items-center pe-3"
+        >
+          {loading ? (
+            <div className="loading loading-spinner"></div>
+          ) : (
+            <BsSend />
+          )}
+        </button>
+      </div>
+    </form>
+  );
+};
+export default MessageInput;
+
+// STARTER CODE SNIPPET
+// import { BsSend } from "react-icons/bs";
+
+// const MessageInput = () => {
+// 	return (
+// 		<form className='px-4 my-3'>
+// 			<div className='w-full'>
+// 				<input
+// 					type='text'
+// 					className='border text-sm rounded-lg block w-full p-2.5  bg-gray-700 border-gray-600 text-white'
+// 					placeholder='Send a message'
+// 				/>
+// 				<button type='submit' className='absolute inset-y-0 end-0 flex items-center pe-3'>
+// 					<BsSend />
+// 				</button>
+// 			</div>
+// 		</form>
+// 	);
+// };
+// export default MessageInput;
---
+++ frontend/src/components/sidebar/LogoutButton.jsx
@@ -0,0 +1,23 @@
+import { BiLogOut } from "react-icons/bi";
+import useLogout from "../../hooks/useLogout";
+import { useAuthContext } from "../../context/AuthContext";
+
+const LogoutButton = () => {
+	const { loading, logout } = useLogout();
+	const { authUser } = useAuthContext();
+	console.log(authUser);
+
+	return (
+		<div className='mt-auto'>
+			{!loading ? (
+				<div className="flex items-center gap-2 justify-between bg-slate-500 bg-opacity-40 px-4 py-2 rounded">
+				<BiLogOut className='w-7 h-7 text-gray-400 hover:text-white cursor-pointer' onClick={logout} />
+				<img className='w-10 h-10 rounded-full' alt='Profile Pic' src={authUser?.profilePic} />
+				</div>
+			) : (
+				<span className='loading loading-spinner'></span>
+			)}
+		</div>
+	);
+};
+export default LogoutButton;
