a56177f1bab56e6b6cba53b2597420aae0081e50
Nitash-Biswas <nitash.nb@gmail.com>
2024-09-07 01:56:34 +0530

    Change Socket.io url

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,8 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("http://localhost:5000", {
+			//http://localhost:5000
+			const socket = io("https://mern-chat-app-nicq.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
